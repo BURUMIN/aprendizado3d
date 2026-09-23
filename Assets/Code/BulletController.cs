2fbc6c973a70c33eceb67dfc58b4cef135de6154
BURUMIN <brunodemoraeswkk@gmail.com>
2026-09-23 12:13:25 -0300

    quem fezfez

---
+++ Assets/Code/BulletController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float velocity;
+    public Vector3 direction;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.linearVelocity = direction*velocity;
+    }
+}
